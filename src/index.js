f17ad6a021a982ea623ceaf29971a08ffd474bc8
Karol Kotański <karolfm12@onet.eu>
2024-01-29 12:04:19 +0100

    markup

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const select = document.querySelector('.breed-select');
+const url = 'https://api.thecatapi.com/v1/breeds';
+
+select.addEventListener('click', e => {
+  e.preventDefault();
+  fetchCats()
+    .then(cats => renderCats(cats))
+    .catch(error => console.log(error));
+});
+
+const fetchCats = () => {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
+
+function renderCats(cats) {
+  const markup = cats
+    .map(cat => {
+      return `<option>${cat.name}</option>`;
+    })
+    .join('');
+  select.insertAdjacentHTML('beforeend', markup);
+}
+
+axios.defaults.headers.common[
+  'live_nMDJKc8zVDAlgSqaHkKd04wrQCeOb5cJYxoNKNEjzHvnsHOOTEg79WBJLZrdfHv2'
+] = 'твой ключ';
